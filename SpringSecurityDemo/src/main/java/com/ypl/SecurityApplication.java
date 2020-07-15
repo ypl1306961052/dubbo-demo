69dd53059dedc40958c87d230b979e0bd36202cb
yangpeilin <yangpeilin@wondergroup.com>
2020-07-15 16:57:10 +0800

    添加忽略文件

---
+++ SpringSecurityDemo/src/main/java/com/ypl/SecurityApplication.java
@@ -0,0 +1,25 @@
+package com.ypl;
+/*
+作者：yangp
+创建时间：2020/7/9-9:54-2020
+*/
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wxt.yangp
+ * @version V1.0
+ * @Title: SecurityApplication
+ * @Package com.ypl
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2020/7/9 9:54
+ */
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+}
---
+++ SpringSecurityDemo/src/main/java/com/ypl/controller/SysController.java
@@ -0,0 +1,19 @@
+package com.ypl.controller;
+/*
+作者：yangp
+创建时间：2020/7/9-9:55-2020
+*/
+
+
+/**
+ * @author wxt.yangp
+ * @version V1.0
+ * @Title: SysController
+ * @Package com.ypl.controller
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2020/7/9 9:55
+ */
+
+
+public class SysController {
+}
